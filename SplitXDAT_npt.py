618bd94c0321c56bbecf27e98c4f0b0b0a665289
ZhendCao <1819148259@qq.com>
2021-07-16 13:24:13 +0800

    update npt

--- SplitXDAT_npt.py
+++ SplitXDAT_npt.py
@@ -6,11 +6,17 @@ def get_atom_num(lines):
 def print_every_poscar(lines,N):
     num_poscar = len(lines)/N           ####number of poscars
     j = 0
+    k = 0
     for i in range(0,len(lines)):
         if i%N == 0:
             j = j + 1
-            outfile = open('POSCAR-%s' % j, 'w')
-        outfile.write(str(lines[i]))    
+            if j > 1000:
+                k = k + 1
+                outfile = open('POSCAR-%s' % k, 'w')
+        if j > 1000:
+            outfile.write(str(lines[i]))
+#	if j >= 5:
+#	    break    
     return 0
 		
 #filepath = "D:\\study\\data\\NPT\\test\\XDATCAR"
